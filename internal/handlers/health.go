bdc89d5912d636f9548d8e276148d4853a739c01
enerzilius <efblouback@gmail.com>
2026-04-17 19:44:58 -0300

    feat: db check on health endpoint

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -4,13 +4,31 @@ import (
 	"encoding/json"
 	"net/http"
 	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	//"internal/env"
 )
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
+
+	connectionString := "host=arthemis-brain-postgres port=5432 user=app_user password=app_password dbname=app_db sslmode=disable"
+
+	_, err := gorm.Open(
+		postgres.Open(connectionString),
+		&gorm.Config{},
+	)
+
+	dbIsUp := "available"
+	if err != nil {
+		dbIsUp = "unavailable"
+	}
+
 	json.NewEncoder(w).Encode(map[string]string{
 		"status": "ok",
+		"db":     dbIsUp,
 		"time":   time.Now().UTC().Format(time.RFC3339),
 	})
 }
